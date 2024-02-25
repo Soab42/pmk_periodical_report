786fecfecd75424ac619618a87942d6eecca4cbc
Soab42 <108150913+Soab42@users.noreply.github.com>
2024-02-25 22:11:01 +0600

    add fbs

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { startScraping } from "./rmc.js";
+import { DateTime } from "luxon";
 import bodyParser from "body-parser";
 import fs from "fs/promises";
 import cors from "cors";
+import { scraping } from "./fbs.js";
+import { fbsReport } from "./fbs_report.js";
 const app = express();
 const port = 3000;
 
@@ -32,6 +35,35 @@ app.post("/api/scraping", async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
+
+app.post("/api/scraping/fbs", async (req, res) => {
+  const { toDate } = req.body;
+  // console.log(fromDate, toDate);
+
+  try {
+    // Require and run the index.js script with fromDate and toDate parameters
+    await scraping(toDate);
+    // await fbsReport();
+    res.send({ message: "scraping successfully completed" });
+  } catch (error) {
+    console.error("Error running script:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/api/scraping/fbs_interest", async (req, res) => {
+  // console.log(fromDate, toDate);
+
+  try {
+    // Require and run the index.js script with fromDate and toDate parameters
+    await fbsReport();
+    res.send({ message: "scraping successfully completed" });
+  } catch (error) {
+    console.error("Error running script:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 // Define a route to fetch and show data from the JSON file
 app.get("/api/data", async (req, res) => {
   try {
@@ -47,7 +79,148 @@ app.get("/api/data", async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
+
+app.get("/api/fbs", async (req, res) => {
+  try {
+    // Read the content of the fbs.json file
+    const data = await fs.readFile("./fbs.json", "utf-8");
+    const jsonData = JSON.parse(data);
+
+    const formattedData = {};
+
+    // Iterate over each object in the JSON data
+    Object.keys(jsonData).forEach((objName) => {
+      const objData = jsonData[objName];
+
+      // Extract the values associated with the keys
+      const branch_info =
+        objData.branch_info || "Branch information not provided";
+      const report_view = objData.report_view || "Report view not provided";
+      const fdr_info = objData.fdr_info || "FDR information not provided";
+      const date_to = objData.date_to || "Date to not provided";
+
+      // Format the data for the current object
+      formattedData[objName] = { branch_info, report_view, fdr_info, date_to };
+    });
+
+    // Send the formatted data in the response
+    res.json(formattedData);
+  } catch (error) {
+    console.error("Error reading JSON file:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/api/fbs/list", async (req, res) => {
+  try {
+    // Read the content of the fbs.json file
+    const data = await fs.readFile("./fbs.json", "utf-8");
+    const jsonData = JSON.parse(data);
+
+    const formattedData = {};
+
+    // Iterate over each object in the JSON data
+    Object.keys(jsonData).forEach((objName) => {
+      const objData = jsonData[objName];
+
+      const fdr_info = Array.isArray(objData.fdr_info) ? objData.fdr_info : [];
+
+      // Format the data for the current object
+      formattedData[objName] = fdr_info;
+    });
+
+    // Send the formatted data in the response
+    res.json(formattedData);
+  } catch (error) {
+    console.error("Error reading JSON file:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/api/fbs/interest/:date", async (req, res) => {
+  try {
+    const requestedDate = req.params.date; // Get the date from the URL parameter
+    const parsedDate = DateTime.fromISO(requestedDate);
+
+    if (!parsedDate.isValid) {
+      // If the provided date is invalid, return a 400 Bad Request response
+      return res
+        .status(400)
+        .send(
+          "Invalid date format. Please provide the date in ISO format (YYYY-MM-DD)."
+        );
+    }
+
+    // Read the content of the fbs.json file
+    const data = await fs.readFile("./fbs_report.json", "utf-8");
+    const jsonData = JSON.parse(data);
+    let interestData = [];
+
+    for (const key in jsonData) {
+      const value = jsonData[key];
+      if (value.saving_schedules) {
+        interestData.push(value);
+      }
+    }
+
+    // Filter out the matching objects whose savings schedule has the requested date
+    const matchingObjects = interestData.filter((obj) => {
+      return obj.saving_schedules.some(
+        (schedule) => schedule.date === requestedDate
+      );
+    });
+
+    // Send the matching objects in the response
+    res.json(matchingObjects);
+  } catch (error) {
+    console.error("Error reading JSON file:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/api/fbs/:id", async (req, res) => {
+  try {
+    // Read the content of the fbs.json file
+    const data = await fs.readFile("./fbs_report.json", "utf-8");
+    const jsonData = JSON.parse(data);
+    const id = req.params.id;
+    const formattedData = jsonData[id];
+    // Send the formatted data in the response
+    res.json(formattedData);
+  } catch (error) {
+    console.error("Error reading JSON file:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
+
+  // available ports
+
+  console.log(`Server is running at http://localhost:${port}/api/scraping`);
+  console.log("");
+  console.log(`Server is running at http://localhost:${port}/api/scraping/fbs`);
+  console.log("toDate");
+  console.log(
+    `Server is running at http://localhost:${port}/api/scraping/fbs_interest`
+  );
+  console.log("");
+  // for 22
+  console.log(`Server is running at http://localhost:${port}/api/data`);
+
+  // for 22
+  console.log(`Server is running at http://localhost:${port}/api/fbs/list`);
+  //for fbs
+  console.log(`Server is running at http://localhost:${port}/api/fbs`);
+
+  //for fbs
+  console.log(
+    `Server is running at http://localhost:${port}/api/fbs/interest/:date`
+  );
+
+  //for fbs member id
+  console.log(`Server is running at http://localhost:${port}/api/fbs/:id`);
+
+  console.log("exam-> /fbs/2024-02-25");
 });
---
+++ fbs.js
@@ -0,0 +1,116 @@
+import { Cluster } from "puppeteer-cluster";
+import * as fs from "fs";
+import { loginCredentials } from "./credentials.js";
+
+async function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export async function scraping(toDate) {
+  const cluster = await Cluster.launch({
+    concurrency: Cluster.CONCURRENCY_CONTEXT, // Run in parallel
+    maxConcurrency: 1, // Number of concurrent puppeteer instances
+    puppeteerOptions: { headless: false, defaultViewport: null },
+  });
+
+  const resultData = {};
+  const jsonData = {};
+  let sl = 0;
+  await cluster.task(async ({ page, data: { username, password } }) => {
+    await page.goto("https://mfnext.microfin360.com/pmk/#/login");
+    await page.setViewport({
+      width: 1600,
+      height: 1200,
+      deviceScaleFactor: 1,
+    });
+    console.log(`starting ${username} data collection .....`);
+    console.log("\n");
+    const usernameInput = await page.$("#__BVID__12");
+    const passwordInput = await page.$("#__BVID__13");
+    const loginButton = await page.$("button.btn:nth-child(3)");
+
+    await usernameInput.type(username);
+    await passwordInput.type(password);
+    await loginButton.click();
+
+    await delay(3000);
+    // console.log("login success and going to 22 point");
+    await page.goto(
+      "https://mfnext.microfin360.com/pmk/#/reports/register-reports/fdr-register-index"
+    );
+    await delay(3000);
+
+    async function scrapePageData(page) {
+      // console.log("scraping started..");
+      try {
+        // await page.type('input[name="txt_date_to"]', toDate);
+
+        // Select the appropriate option in the dropdown
+        // await page.select("#cbo_duration", "1");
+
+        // await searchButton.click();
+        // Wait for a brief moment (you might need to adjust the timing)
+        // Fill out the input field
+        await page.type('input[name="txt_date_to"]', toDate);
+        await page.waitForTimeout(5000);
+
+        // Execute subsequent commands here
+
+        // Example: Click on a button
+        await page.click('button[type="submit"]');
+
+        // console.log("searchButton", searchButton);
+
+        return new Promise(async (resolve) => {
+          // Listen for network responses
+          page.on("response", async (response) => {
+            const url = response.url();
+            // console.log(url);
+
+            if (url.includes("register_reports/ajax_for_fdr_register_report")) {
+              const responseBody = await response.json();
+              // console.log("responseBody", responseBody);
+              jsonData[username] = responseBody; //can i add something that make it json data
+              // console.log(responseBody);
+              if (responseBody) {
+                resolve(jsonData);
+              }
+            }
+          });
+
+          // Close the browser when done
+          // await browser.close();
+        });
+
+        // ... existing scraping logic ...
+      } catch (error) {
+        console.error("An error occurred during scraping:", username, error);
+        return []; // Return an empty array to indicate error
+      }
+    }
+    const tableData = await scrapePageData(page);
+    // console.log("final", tableData);
+    sl = sl + 1;
+    console.log(sl + ".completed " + username);
+    console.log("\n");
+    resultData[username] = tableData;
+  });
+
+  for (const { username, password } of loginCredentials) {
+    cluster.queue({ username, password });
+  }
+
+  await cluster.idle();
+  await cluster.close();
+
+  // Save the collected data as a JSON file
+  fs.writeFileSync("fbs.json", JSON.stringify(jsonData, null, 2));
+  return jsonData;
+}
+// scraping("2024-02-01");
+
+// export function startScraping({ fromDate, toDate }) {
+//   if (fromDate && toDate) {
+//     return scraping(fromDate, toDate);
+//   }
+// }
